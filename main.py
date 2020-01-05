d90aa62a1c3b8044ce6bd6958043e37d040aaba4
Sushi6006 <lin.terry.x2@gmail.com>
2020-01-06 02:30:50 +0800

    finished tf

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import re
 import string
+from datetime import datetime
 from bs4 import BeautifulSoup
 
 # for word processing
@@ -79,25 +80,59 @@ def filter_words(tokens):
     return [word for word in tokens if is_valid(word)]
 
 
-def output(no_string_identified, no_string_filtered, no_word, counter, file_count, fail_count):
+def output(no_string_identified, no_string_filtered, no_word, counter, file_count, fail_count, title):
     NUM_TO_SHOW = NUM_TO_CALC
-    with open("output.txt", "w") as output_file:
-        title = ("\n\n{0}\n"
+    file_name = "_".join(title.lower().split()) + "_output.txt"
+    with open(file_name, "w") as output_file:
+        title = ("\n{0}\n"
+                 "{4}{5:^20}{4}\n"
                  "FILES FOUND: {1};\n"
                  "READ SUCCESSFULLY: {2};   FAILED TO READ: {3};\n"
-                 "{0}\n").format('='*50, fail_count + file_count, file_count, fail_count)
+                 "{0}\n").format('='*50, fail_count + file_count, file_count, fail_count, '='*15, title.upper())
         data_info = ("Number of strings identified:       {}\n"
                      "Number of strings after filtration: {}\n"
                      "Number of individual words:         {}\n\n\n").format(no_string_identified, no_string_filtered, no_word)
         list_title = "      {0:20}   | {1:20}\n".format("MOST COMMON", "LEAST COMMON")
         output_txt = title + data_info + list_title
 
-        for i in range(min(NUM_TO_SHOW, len(counter))):
+        num_listed = min(NUM_TO_SHOW, len(counter))
+        for i in range(num_listed):
             word_1 = counter[i]
-            word_2 = counter[-(NUM_TO_SHOW-i)]
+            word_2 = counter[-(num_listed-i)]
             output_txt += "{0:6}{1:15}{2:>5}   | {3:15}{4:>5}\n".format(str(i + 1) + ".", word_1[0], word_1[1], word_2[0], word_2[1])
 
         output_file.write(output_txt)
+
+
+def tf_output(tf_train, tf_test):
+
+    train_output_file = open("train_tf.txt", "w")
+    test_output_file = open("test_tf.txt", "w")
+
+    title = ("\n\n{0}\n"
+             "{1:^50}\n{0}\n\n").format('='*50, "Term Frequency")
+    table = "{0:>5}|".format("")
+    for i in range(NUM_TO_CALC):
+        table += "{0:>8}".format("word_" + str(i + 1))
+    train_output = test_output = title + table + "\n"
+
+    for i in range(len(tf_train)):
+        train_output += "{0:<5}|".format("d" + str(i + 1))
+        doc = tf_train[i]
+        for f in doc:
+            train_output += "{0:>8}".format(f'{f:.2f}')
+        train_output += "\n"
+
+    for i in range(len(tf_test)):
+        test_output += "{0:<5}|".format("d" + str(i + 1))
+        doc = tf_test[i]
+        for f in doc:
+            test_output += "{0:>8}".format(f'{f:.2f}')
+        test_output += "\n"
+
+    train_output_file.write(train_output)
+    test_output_file.write(test_output)
+
 
 
 def test_output(info, msg):
@@ -108,7 +143,7 @@ def test_output(info, msg):
     print("word_count:    ", info["word_count"])
     print("file_count:    ", info["file_count"])
     print("fail_count:    ", info["fail_count"])
-    print(f'{"="*30}')
+    print(f'{"="*32}')
 
 
 # project 1 main, calculate frequency related stuff
@@ -141,8 +176,6 @@ def calc_main(file_dir, is_file=False):
     sorted_freqs = sorted(c.items(), key=lambda a: (-a[1], a[0]))
     no_word = len(c.keys())
 
-    output(no_string_identified, no_string_filtered, no_word, sorted_freqs, file_count, fail_count)
-
     result = {"str_identified": no_string_identified,
               "str_filtered": no_string_filtered,
               "word_count": no_word,
@@ -167,26 +200,42 @@ def calc_tf(word, freqs):
 #====================================#
 def main():
 
+    begin_time = datetime.now().replace(microsecond=0)
+
     # get results from files
     train_result = calc_main("projectdataset/train")
     test_result = calc_main("projectdataset/test")
+    output(train_result["str_identified"], train_result["str_filtered"], train_result["word_count"],
+           train_result["sorted_freqs"], train_result["file_count"], train_result["fail_count"], "TRAINING SET")
+    output(test_result["str_identified"], test_result["str_filtered"], test_result["word_count"],
+           test_result["sorted_freqs"], test_result["file_count"], test_result["fail_count"], "TESTING SET")
 
     test_output(train_result, "train set")
     test_output(test_result, "test set")
 
-    tf_train = [[0 for j in range(NUM_TO_CALC)] for i in range(train_result["file_count"])]
+    time_at_mark1 = datetime.now().replace(microsecond=0)
+    print("===== TIME USED SO FAR: {}\n".format(time_at_mark1 - begin_time))
 
+    # read the following print statement :)
+    print("===== Calculating TF...")
+    tf_train = [[0 for j in range(NUM_TO_CALC)] for i in range(train_result["file_count"])]
+    tf_test = [[0 for j in range(NUM_TO_CALC)] for i in range(test_result["file_count"])]
     train_fileset = train_result["files_read"]
     test_fileset = test_result["files_read"]
     for i in range(NUM_TO_CALC):
-        curr_word = train_result["sorted_freqs"][i][0]
-        curr_freq = train_result["sorted_freqs"][i][1]
-
+        curr_time = datetime.now().replace(microsecond=0)
+        print("===== WORD no.{0:0>4d} ===== TIME USED: {1:<8} =====".format(i, str(curr_time - begin_time)))
+        train_word = train_result["sorted_freqs"][i][0]
+        test_word = train_result["sorted_freqs"][i][0]
         for j in range(train_result["file_count"]):
             result = calc_main(train_fileset[i], is_file=True)
-            tf_train[j][i] = calc_tf(curr_word, result["sorted_freqs"])
+            tf_train[j][i] = calc_tf(train_word, result["sorted_freqs"])
+        for j in range(test_result["file_count"]):
+            result = calc_main(test_fileset[i], is_file=True)
+            tf_test[j][i] = calc_tf(test_word, result["sorted_freqs"])
 
-    print(tf_train)
+
+    tf_output(tf_train, tf_test)
 
 
 if __name__ == "__main__":
